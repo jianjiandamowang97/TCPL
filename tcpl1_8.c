0b67f5d099710042d714c228eda31993bfbf17ff
HFF0131 <yzl0131@protonmail.com>
2025-05-13 16:13:40 +0800

    统计输入的字符数

--- tcpl1_8.c
+++ tcpl1_8.c
@@ -1,3 +1,14 @@
 //
 // Created by jianjian on 2025/5/13.
 //
+#include <stdio.h>
+int main(void)
+{
+    long nc = 0;
+    while (getchar() != EOF)
+    {
+        nc++;
+    }
+    printf("%ld\n", nc);
+    return 0;
+}
